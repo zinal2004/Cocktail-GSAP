bbe3b5c438a7a829afec153a7ec272beb6da1ea3
zinal2004 <zinal200420@gmail.com>
2025-06-22 18:36:29 +0530

    Implement Hero Section Animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all'
 gsap.registerPlugin(ScrollTrigger, SplitText);
 import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-        <h1 className='text-3xl text-indigo-300'>Hello, GSAP!</h1>
-    </div>
+    <main>
+        <Navbar />
+        <Hero />
+        
+    </main>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react'
+
+import { navLinks } from '../../constants/index.js'
+
+const Navbar = () => {
+ useGSAP(() => {
+	const navTween = gsap.timeline({
+	 scrollTrigger: {
+		trigger: 'nav',
+		start: 'bottom top'
+	 }
+	});
+	
+	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+	 backgroundColor: '#00000050',
+	 backgroundFilter: 'blur(10px)',
+	 duration: 1,
+	 ease: 'power1.inOut'
+	});
+ })
+ 
+ return (
+	<nav>
+	 <div>
+		<a href="#home" className="flex items-center gap-2">
+		 <img src="/images/logo.png" alt="logo" />
+		 <p>Mixology by Z</p>
+		</a>
+		
+		<ul>
+		 {navLinks.map((link) => (
+			<li key={link.id}>
+			 <a href={`#${link.id}`}>{link.title}</a>
+			</li>
+		 ))}
+		</ul>
+	 </div>
+	</nav>
+ )
+}
+export default Navbar
